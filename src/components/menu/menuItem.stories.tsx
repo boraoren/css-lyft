2d9dac362c9147055ffa67e3eb460c0032be74e5
boraoren <boraoren@gmail.com>
2019-10-15 03:53:48 +0000

    chore(menuItem): add menu stories to storybook

--- src/components/menu/menuItem.stories.tsx
+++ src/components/menu/menuItem.stories.tsx
@@ -2,13 +2,22 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import MenuItem from './MenuItem';
 
-const stories = storiesOf('Components', module);
+const menuStories = storiesOf('Components/menu', module);
 
-stories.add(
-  'menu/item',
-  () => (<MenuItem text="MENU ITEM"/>),
+menuStories.add(
+    'menu/item',
+    () => (<MenuItem text="MENU ITEM" />),
 ).add(
-  'menu/driver',
-  () => (<MenuItem text="DRIVER"/>),
+    'menu/item/driver',
+    () => (<MenuItem text="DRIVER" />),
+).add(
+    'menu/item/rider',
+    () => (<MenuItem text="RIDER" />),
+).add(
+    'menu/item/business',
+    () => (<MenuItem text="BUSINESS" />),
+).add(
+    'menu/item/login',
+    () => (<MenuItem text="LOG IN" />),
 )
 
